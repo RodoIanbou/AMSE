09ced8c2c8cd395c9eea66a843c1b7b895a840c7
RodoIanbou <97095045+RodoIanbou@users.noreply.github.com>
2022-02-11 12:31:11 +0100

    ajout exo1

---
+++ tp2/exo2/lib/Exo2.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Exercice2 extends StatefulWidget {
+  const Exercice2({Key? key}) : super(key: key);
+
+  @override
+  _Exercice2State createState() => _Exercice2State();
+}
+
+class _Exercice2State extends State<Exercice2> {}
